de3ceccd702559185de527d1bfc0c5dc393c91c9
lurui <lurui@ipfsforce.com>
2021-03-26 09:08:18 +0800

    与k8s打通

--- saodiseng/src/bin/server/v1alpha2/rpc_version.rs
+++ saodiseng/src/bin/server/v1alpha2/rpc_version.rs
@@ -9,7 +9,7 @@ pub fn version(request:Request<VersionRequest>) -> VersionResponse {
         println!("Got a request: {:?}", request);
         let reply = VersionResponse {
             version:"0.1.0".to_string(),
-            runtime_name:"dragonforce".to_string(),
+            runtime_name:"saodiseng".to_string(),
             runtime_version:"1.20.0".to_string(),
             runtime_api_version:"v1".to_string()
         };
