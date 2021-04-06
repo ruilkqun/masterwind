19c549dd46d1e63519870cdd04bd49d952e04bfa
lurui <lurui@ipfsforce.com>
2021-04-06 19:15:14 +0800

    微调

--- saodiseng/src/bin/server/pull_image.rs
+++ saodiseng/src/bin/server/pull_image.rs
@@ -3,6 +3,8 @@ use tonic::Request;
 use crate::v1alpha2::impl_struct::k8s_v1alpha2;
 use k8s_v1alpha2::{ PullImageRequest,PullImageResponse };
 
+use ant_king_image::cri_server_image_pull::cri_pull_image;
+
 pub fn pull_image(request:Request<PullImageRequest>) -> PullImageResponse {
 
         println!("Got a request: {:?}", request);
