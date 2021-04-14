0965f7392763b1d08a73c2c7e492d04cea46fb76
lurui <lurui@ipfsforce.com>
2021-04-14 16:09:06 +0800

    微调

--- saodiseng/src/bin/server/impl_image_fs_info_v1alpha2.rs
+++ saodiseng/src/bin/server/impl_image_fs_info_v1alpha2.rs
@@ -8,27 +8,32 @@ use ant_king_image::cri_server_image_fs_info::cri_image_fs;
 
 
 pub async fn images_fs_info_impl_v1alpha2(request:Request<ImageFsInfoRequest>) -> ImageFsInfoResponse {
+        let mut result = Vec::new();
         let computer_result = cri_image_fs().await;
         let timestamp = computer_result.0;
 
-        let fs_id = FilesystemIdentifier{
+        let fs_id = Some(FilesystemIdentifier{
             mountpoint:computer_result.1
-        };
+        });
 
-        let used_bytes = UInt64Value {
+        let used_bytes = Some(UInt64Value {
             value: computer_result.2
-        };
+        });
 
-        let inodes_used = UInt64Value {
+        let inodes_used = Some(UInt64Value {
             value: computer_result.3
-        };
+        });
 
-        let reply_tmp = ImageFsInfoResponse {
+        let reply_item = FilesystemUsage {
             timestamp,
-            mountpoint,
+            fs_id,
             used_bytes,
             inodes_used,
         };
 
+        result.push(reply_item);
+        let reply =  ImageFsInfoResponse {
+            image_filesystems: result
+        };
         return reply
 }
